00490acb2243a133869ccc982d76b4b694bf9845
Ulli3101 <ullrich001@gmail.com>
2026-08-27 18:38:51 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'mein-begleiter-persoenlich-cache-v1-9-1';
+const CACHE = 'mein-begleiter-persoenlich-cache-v1-9-1-1';
 const BASIS = '/Mein-Begleiter-Persoenlich/';
 const PFLICHTDATEIEN = [
   BASIS,
